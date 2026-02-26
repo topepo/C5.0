6543af2fdcba15747f220f40810e32c70ff7ae53
‘topepo’ <mxkuhn@gmail.com>
2026-02-26 08:17:42 -0500

    air format

--- R/C5.0.R
+++ R/C5.0.R
@@ -1,6 +1,5 @@
-
 #' @export
-C5.0 <-  function(x, ...) UseMethod("C5.0")
+C5.0 <- function(x, ...) UseMethod("C5.0")
 
 #' C5.0 Decision Trees and Rule-Based Models
 #'
@@ -116,23 +115,29 @@ C5.0 <-  function(x, ...) UseMethod("C5.0")
 #' @rdname C5.0
 #' @importFrom Cubist makeDataFile makeNamesFile QuinlanAttributes
 
-C5.0.default <- function(x,
-                         y,
-                         trials = 1,
-                         rules = FALSE,
-                         weights = NULL,
-                         control = C5.0Control(),
-                         costs = NULL,
-                         ...) {
+C5.0.default <- function(
+  x,
+  y,
+  trials = 1,
+  rules = FALSE,
+  weights = NULL,
+  control = C5.0Control(),
+  costs = NULL,
+  ...
+) {
   funcCall <- match.call(expand.dots = TRUE)
-  if (!is.factor(y))
+  if (!is.factor(y)) {
     stop("C5.0 models require a factor outcome", call. = FALSE)
-  if (is.null(colnames(x)))
+  }
+  if (is.null(colnames(x))) {
     stop("column names are required", call. = FALSE)
+  }
   if (control$bands > 2 & !rules) {
-    warning("rule banding only works with rules; ",
-            "'rules' was changed to TRUE",
-            call. = FALSE)
+    warning(
+      "rule banding only works with rules; ",
+      "'rules' was changed to TRUE",
+      call. = FALSE
+    )
     rules <- TRUE
   }
 
@@ -141,48 +146,60 @@ C5.0.default <- function(x,
   lvl <- levels(y)
   nClass <- length(lvl)
   if (!is.null(costs)) {
-    if (!is.matrix(costs))
+    if (!is.matrix(costs)) {
       stop("'costs' should be a matrix", call. = FALSE)
-    if (ncol(costs) != nClass | nrow(costs) != nClass)
-      stop("'cost should be a ", nClass, "x", nClass,
-           "matrix", call. = FALSE)
+    }
+    if (ncol(costs) != nClass | nrow(costs) != nClass) {
+      stop("'cost should be a ", nClass, "x", nClass, "matrix", call. = FALSE)
+    }
     if (is.null(dimnames(costs))) {
-      warning("no dimnames were given for the cost matrix; ",
-              "the factor levels will be used", call. = FALSE)
+      warning(
+        "no dimnames were given for the cost matrix; ",
+        "the factor levels will be used",
+        call. = FALSE
+      )
       colnames(costs) <- lvl
       rownames(costs) <- lvl
     } else {
-      if (is.null(colnames(costs)) |
-          is.null(rownames(costs)))
+      if (
+        is.null(colnames(costs)) |
+          is.null(rownames(costs))
+      ) {
         stop("both row and column names are needed", call. = FALSE)
+      }
     }
     costString <- makeCostFile(costs)
-  } else
+  } else {
     costString <- ""
+  }
 
   maxtrials <- 100
-  if (trials < 1 | trials > maxtrials)
-    stop("number of boosting iterations must be between 1 and ",
-         maxtrials, call. = FALSE)
+  if (trials < 1 | trials > maxtrials) {
+    stop(
+      "number of boosting iterations must be between 1 and ",
+      maxtrials,
+      call. = FALSE
+    )
+  }
 
-  if (!is.data.frame(x) &
-      !is.matrix(x))
+  if (
+    !is.data.frame(x) &
+      !is.matrix(x)
+  ) {
     stop("x must be a matrix or data frame", call. = FALSE)
+  }
 
   if (inherits(x, "tbl_df")) {
     x <- as.data.frame(x)
   }
 
-  if (!is.null(weights) && !is.numeric(weights))
+  if (!is.null(weights) && !is.numeric(weights)) {
     stop("case weights must be numeric", call. = FALSE)
+  }
 
   ## TODO: add case weights to these files when needed
   namesString <-
-    makeNamesFile(x,
-                  y,
-                  w = weights,
-                  label = control$label,
-                  comments = TRUE)
+    makeNamesFile(x, y, w = weights, label = control$label, comments = TRUE)
   dataString <- makeDataFile(x, y, weights)
 
   Z <- .C(
@@ -240,16 +257,19 @@ C5.0.default <- function(x,
 
   ## Figure out how may trials were actually used.
   modelContent <- strsplit(
-    if (rules)
+    if (rules) {
       Z$rules
-    else
-      Z$tree, "\n"
+    } else {
+      Z$tree
+    },
+    "\n"
   )[[1]]
   entries <- grep("^entries", modelContent, value = TRUE)
   if (length(entries) > 0) {
     actual <- as.numeric(substring(entries, 10, nchar(entries) - 1))
-  } else
+  } else {
     actual <- trials
+  }
 
   if (trials > 1) {
     boostResults <- getBoostResults(Z$output)
@@ -259,11 +279,12 @@ C5.0.default <- function(x,
     ## C5.0.default: no visible binding for global variable 'Data'
     Data <- NULL
     size <-
-      if (!is.null(boostResults))
+      if (!is.null(boostResults)) {
         subset(boostResults, Data == "Training Set")$Size
-    else
-      NA
-  }   else {
+      } else {
+        NA
+      }
+  } else {
     boostResults <- NULL
     size <- length(grep("[0-9])$", strsplit(Z$output, "\n")[[1]]))
   }
@@ -296,12 +317,7 @@ C5.0.default <- function(x,
 #' @rdname C5.0
 #' @importFrom stats na.pass model.extract .getXlevels terms
 C5.0.formula <-
-  function (formula,
-            data,
-            weights,
-            subset,
-            na.action = na.pass,
-            ...)  {
+  function(formula, data, weights, subset, na.action = na.pass, ...) {
     call <- match.call()
 
     m <- match.call(expand.dots = FALSE)
@@ -313,21 +329,19 @@ C5.0.formula <-
 
     y <- model.extract(m, "response")
     wt <- model.extract(m, "weights")
-    if (length(wt) == 0L)
+    if (length(wt) == 0L) {
       wt <- NULL
-    if ("(weights)" %in% colnames(m))
+    }
+    if ("(weights)" %in% colnames(m)) {
       m[, "(weights)"] <- NULL
+    }
 
     m <- m[, -1, drop = FALSE]
-    out <- C5.0.default(x = m,
-                        y = y,
-                        weights = wt,
-                        ...)
+    out <- C5.0.default(x = m, y = y, weights = wt, ...)
     out$call <- call
     out$Terms <- Terms
     out$xlevels <- .getXlevels(Terms, m)
     out
-
   }
 
 #' Control for C5.0 Models
@@ -385,25 +399,30 @@ C5.0.formula <-
 #' summary(treeModel)
 #'
 #' @export
-C5.0Control <- function(subset = TRUE,
-                        ## in C, equals  SUBSET=0,	/* subset tests allowed */
-                        bands = 0,
-                        winnow = FALSE,
-                        noGlobalPruning = FALSE,
-                        CF = 0.25,
-                        minCases = 2,
-                        fuzzyThreshold = FALSE,
-                        sample = 0.0,
-                        seed = sample.int(4096, size = 1) - 1L,
-                        earlyStopping = TRUE,
-                        label = "outcome") {
-  if (CF < 0 | CF > 1)
+C5.0Control <- function(
+  subset = TRUE,
+  ## in C, equals  SUBSET=0,	/* subset tests allowed */
+  bands = 0,
+  winnow = FALSE,
+  noGlobalPruning = FALSE,
+  CF = 0.25,
+  minCases = 2,
+  fuzzyThreshold = FALSE,
+  sample = 0.0,
+  seed = sample.int(4096, size = 1) - 1L,
+  earlyStopping = TRUE,
+  label = "outcome"
+) {
+  if (CF < 0 | CF > 1) {
     stop("confidence level must between 0 and 1", call. = FALSE)
-  if (sample < 0.0 | sample > .999)
+  }
+  if (sample < 0.0 | sample > .999) {
     stop("sampling percentage must be between 0.0 and .999", call. = FALSE)
+  }
 
-  if (bands == 1 | bands > 10000)
+  if (bands == 1 | bands > 10000) {
     stop("if used, bands must be between 2 and 10000", call. = FALSE)
+  }
 
   list(
     subset = subset,
@@ -423,20 +442,26 @@ C5.0Control <- function(subset = TRUE,
 
 #' @export
 print.C5.0 <- function(x, ...) {
-  cat("\nCall:\n",
-      truncateText(deparse(x$call, width.cutoff = 500)),
-      "\n\n", sep = "")
+  cat(
+    "\nCall:\n",
+    truncateText(deparse(x$call, width.cutoff = 500)),
+    "\n\n",
+    sep = ""
+  )
 
-  if (x$rbm)
+  if (x$rbm) {
     cat("Rule-Based Model\n")
-  else
+  } else {
     cat("Classification Tree\n")
+  }
 
-  cat("Number of samples:",
-      x$dims[1],
-      "\nNumber of predictors:",
-      x$dims[2],
-      "\n\n")
+  cat(
+    "Number of samples:",
+    x$dims[1],
+    "\nNumber of predictors:",
+    x$dims[2],
+    "\n\n"
+  )
 
   if (x$trials["Requested"] > 1) {
     if (x$trials[1] == x$trials[2]) {
@@ -450,41 +475,62 @@ print.C5.0 <- function(x, ...) {
         "used due to early stopping\n"
       )
     }
-    if (!all(is.na(x$size)))
+    if (!all(is.na(x$size))) {
       cat(
         ifelse(x$rbm, "Average number of rules:", "Average tree size:"),
         round(mean(x$size, na.rm = TRUE), 1),
         "\n\n"
       )
-    else
+    } else {
       cat("\n")
-  } else
+    }
+  } else {
     cat(ifelse(x$rbm, "Number of Rules:", "Tree size:"), x$size, "\n\n")
+  }
 
   otherOptions <- NULL
-  if (x$control$subset)
+  if (x$control$subset) {
     otherOptions <- c(otherOptions, "attempt to group attributes")
-  if (x$control$winnow)
+  }
+  if (x$control$winnow) {
     otherOptions <- c(otherOptions, "winnowing")
-  if (x$control$noGlobalPruning)
+  }
+  if (x$control$noGlobalPruning) {
     otherOptions <- c(otherOptions, "no global pruning")
-  if (x$control$CF != 0.25)
-    otherOptions <- c(otherOptions,
-                      paste("confidence level: ", x$control$CF, sep = ""))
-  if (x$control$minCases != 2)
-    otherOptions <- c(otherOptions,
-                      paste("minimum number of cases: ", x$control$minCases, sep = ""))
-  if (x$control$fuzzyThreshold)
+  }
+  if (x$control$CF != 0.25) {
+    otherOptions <- c(
+      otherOptions,
+      paste("confidence level: ", x$control$CF, sep = "")
+    )
+  }
+  if (x$control$minCases != 2) {
+    otherOptions <- c(
+      otherOptions,
+      paste("minimum number of cases: ", x$control$minCases, sep = "")
+    )
+  }
+  if (x$control$fuzzyThreshold) {
     otherOptions <- c(otherOptions, "fuzzy thresholds")
-  if (x$control$bands > 0)
-    otherOptions <- c(otherOptions,
-                      paste(x$control$bands, " utility bands", sep = ""))
-  if (!x$control$earlyStopping &
-      x$trials["Requested"] > 1)
+  }
+  if (x$control$bands > 0) {
+    otherOptions <- c(
+      otherOptions,
+      paste(x$control$bands, " utility bands", sep = "")
+    )
+  }
+  if (
+    !x$control$earlyStopping &
+      x$trials["Requested"] > 1
+  ) {
     otherOptions <- c(otherOptions, "early stopping for boosting")
-  if (x$control$sample > 0)
-    otherOptions <- c(otherOptions,
-                      paste(round(100 * x$control$sample, 1), "% sub-sampling", sep = ""))
+  }
+  if (x$control$sample > 0) {
+    otherOptions <- c(
+      otherOptions,
+      paste(round(100 * x$control$sample, 1), "% sub-sampling", sep = "")
+    )
+  }
   if (!is.null(otherOptions)) {
     cat(truncateText(paste(
       "Non-standard options:",
@@ -504,11 +550,11 @@ print.C5.0 <- function(x, ...) {
     out <- strsplit(output[sizeIndex + 2], " ")[[1]]
     out <- out[!(out %in% c("\t", ""))]
     out <- out[!grepl("[[:punct:]]", out)]
-    if (length(out) > 0)
+    if (length(out) > 0) {
       cat("Tree Size: ", out, "\n")
+    }
   }
 }
-
 
 
 #' Summaries of C5.0 Models
@@ -555,28 +601,32 @@ print.C5.0 <- function(x, ...) {
 #' @method summary C5.0
 
 summary.C5.0 <- function(object, ...) {
-    out <- list(output = object$output, call = object$call)
-    class(out) <- "summary.C5.0"
-    out
-  }
+  out <- list(output = object$output, call = object$call)
+  class(out) <- "summary.C5.0"
+  out
+}
 
 #' @export
 print.summary.C5.0 <- function(x, ...) {
-    cat("\nCall:\n",
-        truncateText(deparse(x$call, width.cutoff = 500)),
-        "\n\n",
-        sep = "")
-    cat(x$output)
-    cat("\n")
-    invisible(x)
-  }
+  cat(
+    "\nCall:\n",
+    truncateText(deparse(x$call, width.cutoff = 500)),
+    "\n\n",
+    sep = ""
+  )
+  cat(x$output)
+  cat("\n")
+  invisible(x)
+}
 
 truncateText <- function(x) {
-  if (length(x) > 1)
+  if (length(x) > 1) {
     x <- paste(x, collapse = "")
+  }
   w <- options("width")$width
-  if (nchar(x) <= w)
+  if (nchar(x) <= w) {
     return(x)
+  }
 
   cont <- TRUE
   out <- x
@@ -586,20 +636,20 @@ truncateText <- function(x) {
 
     spaceIndex <- gregexpr("[[:space:]]", tmp2)[[1]]
     stopIndex <- spaceIndex[length(spaceIndex) - 1] - 1
-    tmp <- c(substring(tmp2, 1, stopIndex),
-             substring(tmp, stopIndex + 1))
+    tmp <- c(substring(tmp2, 1, stopIndex), substring(tmp, stopIndex + 1))
     out <-
-      if (length(out) == 1)
+      if (length(out) == 1) {
         tmp
-    else
-      c(out[1:(length(x) - 1)], tmp)
-    if (all(nchar(out) <= w))
+      } else {
+        c(out[1:(length(x) - 1)], tmp)
+      }
+    if (all(nchar(out) <= w)) {
       cont <- FALSE
+    }
   }
 
   paste(out, collapse = "\n")
 }
-
 
 
 #' Variable Importance Measures for C5.0 Models
@@ -651,138 +701,141 @@ truncateText <- function(x) {
 #' C5imp(treeModel, metric = "splits")
 #'
 #' @export
-C5imp <- function(object,
-                  metric = "usage",
-                  pct = TRUE,
-                  ...) {
-  if (!(metric %in% c("usage", "splits")))
+C5imp <- function(object, metric = "usage", pct = TRUE, ...) {
+  if (!(metric %in% c("usage", "splits"))) {
     stop("metric should be either 'usage' or 'splits'")
+  }
   allVar <- getOriginalVars(object)
   allVar <- gsub("\\", "", allVar, fixed = TRUE)
   if (metric == "usage") {
     object$output <- strsplit(object$output, "\n")[[1]]
     usageIndex <-
       grep("Attribute usage:", object$output, fixed = TRUE)
-    if (length(usageIndex) == 0)
+    if (length(usageIndex) == 0) {
       stop("Error in parsing model output")
+    }
     object$output <-
       object$output[usageIndex:length(object$output)]
     usageData <-
       grep("%\t", object$output, fixed = TRUE, value = TRUE)
 
     usageData <- strsplit(usageData, "%", fixed = TRUE)
-    if (!all(unlist(lapply(usageData, length)) == 2))
+    if (!all(unlist(lapply(usageData, length)) == 2)) {
       stop("Error in parsing model output")
+    }
 
     usageData <-
-      lapply(usageData, function(x)
-        gsub("[[:blank:]]", "", x))
+      lapply(usageData, function(x) {
+        gsub("[[:blank:]]", "", x)
+      })
     usageData <-
       as.data.frame(do.call("rbind", usageData), stringsAsFactors = FALSE)
     elim <- allVar[!(allVar %in% usageData$V2)]
     if (length(elim) > 0) {
-      elimVars <- data.frame(V1 = 0,
-                             V2 = elim,
-                             stringsAsFactors = FALSE)
+      elimVars <- data.frame(V1 = 0, V2 = elim, stringsAsFactors = FALSE)
       usageData <- rbind(usageData, elimVars)
     }
     out <-
-      data.frame(Overall =  as.numeric(as.character(usageData$V1)))
-    rownames(out) <-  usageData$V2
+      data.frame(Overall = as.numeric(as.character(usageData$V1)))
+    rownames(out) <- usageData$V2
   } else {
     varData <- strsplit(paste(object$tree, object$rules), "\n")[[1]]
     varData <- grep("att=", varData, value = TRUE)
     varData <- breakUp(varData)
-    varData <- unlist(lapply(varData, function(x)
-      x["att"]))
+    varData <- unlist(lapply(varData, function(x) {
+      x["att"]
+    }))
 
     varData <-
       as.data.frame(table(varData), stringsAsFactors = FALSE)
     elim <- allVar[!(allVar %in% varData$varData)]
-    if (length(elim) > 0)
-    {
+    if (length(elim) > 0) {
       elimVars <-
-        data.frame(varData = elim,
-                   Freq = 0,
-                   stringsAsFactors = FALSE)
+        data.frame(varData = elim, Freq = 0, stringsAsFactors = FALSE)
       varData <- rbind(varData, elimVars)
     }
     out <-
-      data.frame(Overall =  as.numeric(as.character(varData$Freq)))
-    if (pct)
+      data.frame(Overall = as.numeric(as.character(varData$Freq)))
+    if (pct) {
       out$Overall <- out$Overall / sum(out$Overall) * 100
-    rownames(out) <-  varData$varData
+    }
+    rownames(out) <- varData$varData
   }
   out[order(out$Overall, decreasing = TRUE), , drop = FALSE]
 }
 
 breakUp <- function(y) {
-    y <- gsub("\"", "", y)
-    y <- strsplit(y, " ", fixed = TRUE)
-    y <- lapply(
-      y,
-      function(z) {
-        z <- strsplit(z, "=", fixed = TRUE)
-        nms <- unlist(lapply(z, function(a) a[1]))
-        val <- unlist(lapply(z, function(a) a[2]))
-        names(val) <- nms
-        val
-      }
-    )
-   y
-  }
+  y <- gsub("\"", "", y)
+  y <- strsplit(y, " ", fixed = TRUE)
+  y <- lapply(
+    y,
+    function(z) {
+      z <- strsplit(z, "=", fixed = TRUE)
+      nms <- unlist(lapply(z, function(a) a[1]))
+      val <- unlist(lapply(z, function(a) a[2]))
+      names(val) <- nms
+      val
+    }
+  )
+  y
+}
 
 
 getOriginalVars <- function(x) {
   treeDat <- strsplit(x$names, "\n")[[1]]
-  varStart <- grep(paste(x$control$label, ":", sep = ""),
-                   treeDat)
-  if (length(varStart) == 0)
+  varStart <- grep(paste(x$control$label, ":", sep = ""), treeDat)
+  if (length(varStart) == 0) {
     stop("cannot parse names file")
+  }
   treeDat <- treeDat[(varStart + 1):length(treeDat)]
   treeDat <- strsplit(treeDat, ":")
-  unlist(lapply(treeDat, function(x)
-    x[1]))
+  unlist(lapply(treeDat, function(x) {
+    x[1]
+  }))
 }
 
-getVars <- function(x)
-{
+getVars <- function(x) {
   ## One of these is always ""
   treeDat <- paste(x$tree, x$rules)
   treeDat <- strsplit(treeDat, "\n")[[1]]
   treeDat <- grep("att=", treeDat, value = TRUE)
   treeDat
-
 }
 
 getAtt <- function(x) {
   strt <- regexpr("att=", x)
-  if (length(strt) == 0)
+  if (length(strt) == 0) {
     stop("cannot parse model file")
+  }
   strt <- strt + 5
   stp <- regexpr("(forks=)|(cut=)|(val=)", x)
-  if (length(stp) == 0)
+  if (length(stp) == 0) {
     stop("cannot parse model file")
+  }
   stp <- stp - 3
   substring(x, strt, stp)
 }
 
-C5predictors <- function(x, ...)
+C5predictors <- function(x, ...) {
   unique(getAtt(getVars(x)))
+}
 
 getBoostResults <- function(x) {
-  output <-  strsplit(x, "\n")[[1]]
+  output <- strsplit(x, "\n")[[1]]
   ## what above when sampling is used
   srt <- grep("^Trial\t", output)
   stp <- grep("^boost\t", output)
 
   ## error check for srt, stp
-  if (length(srt) == 0 | length(stp) == 0)
+  if (length(srt) == 0 | length(stp) == 0) {
     return(NULL)
-  if (any(stp - srt <= 0))
+  }
+  if (any(stp - srt <= 0)) {
     return(NULL)
-  if (length(srt) != length(stp))
+  }
+  if (length(srt) != length(stp)) {
     return(NULL)
+  }
 
   if (length(stp) > 1) {
     trainSrt <- grep("Evaluation on training data", output)
@@ -803,16 +856,15 @@ getBoostResults <- function(x) {
     boostResults$Data <- "Training Set"
   }
   boostResults
-
-
 }
 
 parseBoostTable <- function(x) {
   x <- gsub("(", " ", x, fixed = TRUE)
   x <- gsub("%)", "", x, fixed = TRUE)
   x <- strsplit(x, "[[:space:]]")
-  x <- lapply(x, function(x)
-    x[x != ""])
+  x <- lapply(x, function(x) {
+    x[x != ""]
+  })
 
   if (all(unlist(lapply(x, length)) %in% 4:5)) {
     x <- do.call("rbind", x)
@@ -820,8 +872,9 @@ parseBoostTable <- function(x) {
     x <- as.data.frame(x)
     cls <- c("Trial", "Size", "Errors", "Percent", "Cost")
     colnames(x) <- cls[1:ncol(x)]
-    x$Trial <-  x$Trial + 1
-  } else
+    x$Trial <- x$Trial + 1
+  } else {
     x <- NULL
+  }
   x
 }
--- R/predict.C5.0.R
+++ R/predict.C5.0.R
@@ -69,39 +69,53 @@
 #' @rawNamespace export(predict.C5.0)
 #' @importFrom Cubist makeDataFile makeNamesFile QuinlanAttributes
 predict.C5.0 <-
-  function (object,
-            newdata = NULL,
-            trials = object$trials["Actual"],
-            type = "class",
-            na.action = na.pass,
-            ...)  {
-    if (!(type %in% c("class", "prob")))
-      stop("type should be either 'class' or 'prob'",
-           call. = FALSE)
-    if (object$cost != "" &
-        type == "prob")
-      stop("confidence values (i.e. class probabilities) should ", "
+  function(
+    object,
+    newdata = NULL,
+    trials = object$trials["Actual"],
+    type = "class",
+    na.action = na.pass,
+    ...
+  ) {
+    if (!(type %in% c("class", "prob"))) {
+      stop("type should be either 'class' or 'prob'", call. = FALSE)
+    }
+    if (
+      object$cost != "" &
+        type == "prob"
+    ) {
+      stop(
+        "confidence values (i.e. class probabilities) should ",
+        "
            not be used with costs",
-           call. = FALSE)
-    if (is.null(newdata))
+        call. = FALSE
+      )
+    }
+    if (is.null(newdata)) {
       stop("newdata must be non-null", call. = FALSE)
+    }
 
     if (!is.null(object$Terms)) {
       object$Terms <- delete.response(object$Terms)
       newdata <-
-        model.frame(object$Terms,
-                    newdata,
-                    na.action = na.action,
-                    xlev = object$xlevels)
-    } else
+        model.frame(
+          object$Terms,
+          newdata,
+          na.action = na.action,
+          xlev = object$xlevels
+        )
+    } else {
       newdata <- newdata[, object$predictors, drop = FALSE]
+    }
 
-    if (is.null(colnames(newdata)))
+    if (is.null(colnames(newdata))) {
       stop("column names are required", call. = FALSE)
+    }
 
-    if (length(trials) > 1)
+    if (length(trials) > 1) {
       stop("only one value of trials is allowed")
-    if (trials > object$trials["Actual"])
+    }
+    if (trials > object$trials["Actual"]) {
       warning(
         paste(
           "'trials' should be <=",
@@ -112,14 +126,16 @@ predict.C5.0 <-
         ),
         call. = FALSE
       )
+    }
 
     ## If there are case weights used during training, the C code
     ## will expect a column of weights in the new data but the
     ## values will be ignored. `makeDataFile` puts those last in
     ## the data when `C5.0.default` is run, so we will add a
     ## column of NA values at the end here
-    if (object$caseWeights)
+    if (object$caseWeights) {
       newdata$case_weight_pred <- NA
+    }
 
     ## make cases file
     caseString <- makeDataFile(x = newdata, y = NULL)
@@ -127,10 +143,12 @@ predict.C5.0 <-
     ## When passing trials to the C code, convert to
     ## zero if the original version of trials is used
 
-    if (trials <= 0)
+    if (trials <= 0) {
       stop("'trials should be a positive integer", call. = FALSE)
-    if (trials == object$trials["Actual"])
+    }
+    if (trials == object$trials["Actual"]) {
       trials <- 0
+    }
 
     ## Add trials (not object$trials) as an argument
     Z <- .C(
@@ -146,18 +164,18 @@ predict.C5.0 <-
       output = character(1),
       PACKAGE = "C50"
     )
-    if(any(grepl("Error limit exceeded", Z$output)))
+    if (any(grepl("Error limit exceeded", Z$output))) {
       stop(Z$output, call. = FALSE)
+    }
 
     if (type == "class") {
       out <- factor(object$levels[Z$pred], levels = object$levels)
     } else {
       out <-
-        matrix(Z$confidence,
-               ncol = length(object$levels),
-               byrow = TRUE)
-      if (!is.null(rownames(newdata)))
+        matrix(Z$confidence, ncol = length(object$levels), byrow = TRUE)
+      if (!is.null(rownames(newdata))) {
         rownames(out) <- rownames(newdata)
+      }
       colnames(out) <- object$levels
     }
     out
